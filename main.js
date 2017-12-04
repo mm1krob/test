9e27a7380b2867357f7c47a09a75791c009dbb5e
mm1krob <mmikrob@gmail.com>
2017-12-04 08:08:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('button').click(function () {
+        let user = {};
+        for (let i = 0; i < 4; i++) {
+            user[document.forms[0].elements[i].name] = document.forms[0].elements[i].value;
+        }
+        user = JSON.stringify(user);
+       emailValidAndSet(document.forms[0].elements.email.value, user);
+    });
+    showUsers();
+
+    function showUsers () {
+        for (let key in localStorage){
+            let person = JSON.parse(localStorage.getItem(key));
+            if(person.full_name=="")person.full_name='нема імені'
+            $('#users').append(`<p><span>${person.full_name}</span> <span>${person.email}</span></p>`);
+        }
+    }
+
+    function emailValidAndSet(email, user){
+        if(email != '') {
+            let pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if(!pattern.test(email)){
+                $('[name="email"]').css({'border' : '1px solid #ff0000'});
+                $('span, span br').remove();
+                $('[name="email"]').after('<span><br />неправильний емейл</span>');
+                $('span').css('color','red');
+            }else if(email in localStorage){
+                $('span, span br').remove();
+                $($('[name="email"]')).after('<span><br />такий користувач вже є, введіть інше імя</span>');
+                $('span').css('color','red');
+
+            }
+             else{
+                localStorage.setItem(document.forms[0].elements.email.value , user);
+                $('span, span br').remove();
+                $('[name="email"]').css({'border' : '1px solid grey'});
+                document.forms[0].reset();
+            }
+        }
+    };
+});
