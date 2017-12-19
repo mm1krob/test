138122c04b02934da3aa51a5470d962d5284eec8
mm1krob <mmikrob@gmail.com>
2017-12-19 14:48:25 +0200

    added expetions and module

--- task3/distance_between_cities.rb
+++ task3/distance_between_cities.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 
+module DestinationHelper
 class Https
 	def initialize(city1,city2)
 		url = URI.parse("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=#{city1}&destinations=#{city2}&key=AIzaSyCY05U4IKsK-Wkb38WwyZOKPgOH6s8j_JI");
@@ -15,24 +16,32 @@ class Https
 		return @response.body;
 	end
 end
+end
+
 
 class DestinationParser
+	def validArg(arg1,arg2)
+		if  arg1=='' or arg2=='' 
+			raise ArgumentError, 'Argument must be field'
+		elsif arg1.is_a? Numeric  or arg2.is_a? Numeric
+			raise C, 'Argument is not string'
+		end
+	end
+	
  	def distance_between_cities(origins, destinations);
-		if destinations.class!=String || origins.class!=String
-			puts 'error: destination or origins should be a string'
-		elsif origins=='' || destinations==''
-			puts 'error: destination or origins should be filled'
-		else
-			
-			net = Https.new(origins, destinations)
+			begin
+			self.validArg(origins, destinations)
+			rescue ArgumentError => er
+			puts er.message + '! Cities going to set by defalt(lviv, kiev)'
+			origins='lviv'
+			destinations='kiev'
+			end
+			net = DestinationHelper::Https.new(origins, destinations)
 			js = JSON.parse(net.getResponse);
 			puts "#{origins} - #{destinations}, #{js['rows'][0]['elements'][0]['distance']['text']}, #{js['rows'][0]['elements'][0]['duration']['text']}";
-		end
+		
 	end
 end
 blala=DestinationParser.new;
 
-blala.distance_between_cities('lviv', 'kiev');
-
-
-
+blala.distance_between_cities('', 'kiev');
